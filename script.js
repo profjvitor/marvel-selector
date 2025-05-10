db89df96274c7b67c1efb224e679b4da66c648a7
João Vítor de Andrade Marcelino <jvitorandrade@prof.educacao.sp.gov.br>
2025-05-10 18:36:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const movies = [
-  { title: "X-Men", year: 2000, franchise: "X-Men", poster: "https://image.tmdb.org/t/p/w500/fmYxjD5JzO0IqdrcNCtnsZ9noC1.jpg" },
+  { title: "X-Men", year: 2000, franchise: "X-Men", poster: "https://a.ltrbxd.com/resized/sm/upload/ub/6i/kh/7q/8259CEB5-947B-4A88-9848-AE748636B747-0-1000-0-1500-crop.jpg?v=58f85fcb62.jpg" },
   { title: "X-Men 2", year: 2003, franchise: "X-Men", poster: "https://image.tmdb.org/t/p/w500/uKDEZtxl9zoiEVbZZsLkZf3PjUu.jpg" },
   { title: "Blade II", year: 2002, franchise: "Blade", poster: "https://image.tmdb.org/t/p/w500/wDd6Kc2Xy8cUepLZpACz5VdUrsE.jpg" },
   { title: "Blade Trinity", year: 2004, franchise: "Blade", poster: "https://image.tmdb.org/t/p/w500/9t2Om3V18xcxtOCz3R5GzA3PrU1.jpg" },
